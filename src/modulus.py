f8d1c79a75e8b08741e49c18194479820fe74ec5
JustWhit3 <biancogianluca9@gmail.com>
2024-11-18 23:35:05 +0100

    Minor improvements

--- src/modulus.py
+++ src/modulus.py
@@ -8,18 +8,21 @@ import shutil
 from logger import logger
 from shell import is_admin
 
+
 def main():
     # Basic declarations
     base_path = "packages"
     configure = ["cmake", "-B", "build"]
     build = ["cmake", "--build", "build"]
     install = ["cmake", "--build", "build", "--target", "install"]
-    
+
     # Check if C++ library is in the archive
-    if not os.path.isdir(f"{base_path}/{args.package}"):
-        logger.error(f"\"{args.package}\" package is not in the archive. Please visit https://github.com/JustWhit3/modulus/tree/main/packages for a list of the available packages.")
+    if not os.path.isdir(f"{base_path}/{args.package}/{args.version}"):
+        logger.error(
+            f'"{args.package}" package v{args.version} is not in the archive. Please visit https://github.com/JustWhit3/modulus/tree/main/packages for a list of the available packages and send a PR if you want.'
+        )
         return
-    
+
     # Install the library
     if is_admin():
         for package in os.listdir(base_path):
@@ -27,26 +30,53 @@ def main():
                 for version in os.listdir(f"{base_path}/{package}"):
                     if version == args.version:
                         complete_path = f"{base_path}/{package}/{version}"
+                        logger.info("configuring...")
+                        subprocess.run(configure, check=True, cwd=complete_path)
 
-                        res = subprocess.run(configure, check=True, cwd=complete_path, text=True, capture_output=True)
-                        if not "is already installed" in res.stdout and not "is already installed" in res.stderr:
-                            print(res.stdout)
-                            subprocess.run(build, check=True, cwd=complete_path)
+                        logger.info("building...")
+                        subprocess.run(build, check=True, cwd=complete_path)
+
+                        if args.install == "yes":
+                            logger.info("installing...")
                             subprocess.run(install, check=True, cwd=complete_path)
-                        else:
-                            logger.info(f"package \"{package}\" with version \"{version}\" is already installed.")
+
+                        # logger.info(
+                        #     f'package "{package}" with version "{version}" is already installed.'
+                        # )
+                        logger.info("cleaning...")
                         shutil.rmtree(f"{complete_path}/build")
+                        logger.info("process complete.")
     else:
         logger.error("admin privileges are required to install packages.")
 
 
 if __name__ == "__main__":
     # Parser settings
-    parser = argparse.ArgumentParser(description="Modulus is an open-source package manager for C++ libraries.")
-    parser.add_argument("-p", "--package", help="The package name to be installed.", type=str, default="None")
-    parser.add_argument("-v", "--version", help="The package version to be installed.", type=str, default="0.0.0")
+    parser = argparse.ArgumentParser(
+        description="Modulus is an open-source package manager for C++ libraries."
+    )
+    parser.add_argument(
+        "-p",
+        "--package",
+        help="The package name to be installed.",
+        type=str,
+        default="None",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="The package version to be installed.",
+        type=str,
+        default="0.0.0",
+    )
+    parser.add_argument(
+        "-i",
+        "--install",
+        help="Choose if installing or not.",
+        type=str,
+        default="yes",
+    )
     args = parser.parse_args()
-    
+
     # Main code
     main()
-    
